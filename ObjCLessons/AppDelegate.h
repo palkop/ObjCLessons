8415faec51dd903ce1c77ad024c277b38436c690
Peter Palko <palko.p@gmail.com>
2013-07-26 13:56:20 -0700

    First commit

---
+++ ObjCLessons/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ObjCLessons
+//
+//  Created by Peter Palko on 7/25/13.
+//  Copyright (c) 2013 Peter Palko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
